48039e1ecdf7b7299558bc2b91dc33f00df9ac70
Fokke Dijkstra <f.dijkstra@rug.nl>
2019-10-23 14:06:37 +0200

    Made the coordinates a variable. Changed srun into mpirun to make it more portable.

--- jobscript.sh
+++ jobscript.sh
@@ -6,4 +6,15 @@
 module purge
 module load foss/2019a
 
-srun ./mandelbrot -a 1.0 -x -0.835 -y 0.20001000001 -z 100 -f 0.8 -n 10000
+# Interesting coordinates
+xc=-0.995246804477361527265
+yc=0.294453898173439620240
+
+#xc=0.3750001200618655
+#yc=-0.2166393884377127
+
+#xc=-0.13856524454488
+#yc=-0.64935990748190
+
+mpirun -n 5 ./mandelbrot -a 2.0 -x $xc  -y $yc -z 100 -f 0.8 -n 10000
+
