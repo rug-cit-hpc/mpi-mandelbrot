35ac450124387a2c7578dbde98b5c58cb32b0750
Fokke Dijkstra <fokke@bobolink>
2019-10-23 09:32:55 +0100

    Updated jobscript for foss/2019a and more nodes.

--- jobscript.sh
+++ jobscript.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --tasks-per-node=1
-##SBATCH --mem=1M
+#SBATCH --nodes=7
+#SBATCH --tasks-per-node=4
 #SBATCH --time=00:30:00
 
 module purge
-module load foss/2018a
+module load foss/2019a
 
-srun ./mandelbrot -a 1.0 -x -0.835 -y 0.20001 -z 100 -f 0.51
+srun ./mandelbrot -a 1.0 -x -0.835 -y 0.20001000001 -z 100 -f 0.8 -n 10000
